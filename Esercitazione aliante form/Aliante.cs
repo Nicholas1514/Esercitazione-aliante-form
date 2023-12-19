6a297fa59410f61dea532cf7760f16995ddcb508
Zappa.20201 <Zappa.20201@E2-9-PC13>
2023-12-19 13:27:54 +0100

    Aggiungere i file di progetto.

---
+++ Esercitazione aliante form/Aliante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione_aliante_form
+{
+	public class Aliante
+	{
+		public Ala ala { get; set; }
+		public Fusoliera fusoliera { get; set; }
+		public Coda coda { get; set; }
+		
+	}
+
+}

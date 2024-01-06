5e7399a6fd0b43738bb809da9bc66fa22d5abad4
ezioc <ezioc@DESKTOP-OIS0K39>
2024-01-06 15:40:30 +0100

    Classe Ala completata

--- Esercitazione aliante form/Ala.cs
+++ Esercitazione aliante form/Ala.cs
@@ -6,8 +6,64 @@ using System.Threading.Tasks;
 
 namespace Esercitazione_aliante_form
 {
-	public class Ala:Aliante
+	public class Ala: IComposite
 	{
-		
-	}
+		private double _lu;
+		private double _ap;
+
+		public double Lu
+		{
+			get { return _lu; }
+			set { _lu = value; }
+		}
+
+		public double Ap
+		{
+			get { return _ap; }
+			set { _ap = value; }
+		}
+
+		public Ala()
+		{
+			Ap = 0;
+			Lu = 0;
+		}
+
+		public Ala(double lu, double ap)
+		{
+			Lu = lu;
+			Ap = ap;
+		}
+
+		public Ala(Ala vala)
+		{
+			Lu = vala.Lu;
+			Ap = vala.Ap;
+		}
+
+        public void Aggiunta(IComposite composite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rimuovi(int indice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComposite GetChild(int indice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Prezzo()
+        {
+            return Ap * Lu;
+        }
+
+        public override string ToString()
+        {
+			return $"Lunghezza: {Lu} + Apertura: {Ap}";
+        }
+    }
 }

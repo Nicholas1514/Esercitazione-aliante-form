e264a634910ff6ab13173d25d57ab62ab02ca4cc
ezioc <ezioc@DESKTOP-OIS0K39>
2024-01-06 15:50:36 +0100

    Classe Coda completata

--- Esercitazione aliante form/Coda.cs
+++ Esercitazione aliante form/Coda.cs
@@ -6,7 +6,72 @@ using System.Threading.Tasks;
 
 namespace Esercitazione_aliante_form
 {
-	public class Coda
+	public class Coda:IComposite
 	{
-	}
+        private double _lung;
+
+        public double Lung
+        {
+            get { return _lung; }
+            set
+            {
+              
+                    _lung = value;
+             
+                    _lung = 8F;
+            }
+        }
+
+        public Coda()
+        {
+            Lung = 0;
+        }
+
+        public Coda(double lung)
+        {
+            Lung = lung;
+        }
+
+        public Coda(Coda vCoda)
+        {
+            Lung = vCoda.Lung;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Coda) || obj == null)
+            {
+                return false;
+            }
+
+            Coda altro = (Coda)obj;
+            return Lung == altro.Lung;
+        }
+
+        public void Aggiunta(IComposite component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rimuovi(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComposite GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Lunghezza coda: {Lung}";
+        }
+
+        public double Prezzo()
+        {
+            return Lung * 30;
+        }
+    }
 }
+

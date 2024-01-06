728802cf1bf1bb45e7ef285fb2746816e136c0f6
ezioc <ezioc@DESKTOP-OIS0K39>
2024-01-06 15:54:28 +0100

    Classi Cerchione e Ruota completate

--- Esercitazione aliante form/Ruota.cs
+++ Esercitazione aliante form/Ruota.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace Esercitazione_aliante_form
 {
-	public class Ruota
+	public class Ruota:IComposite
 	{
-	}
+        private Gomma _gomma;
+        private Cerchione _cerchione;
+
+        public Gomma Gomma
+        {
+            get { return _gomma; }
+            set { _gomma = value; }
+        }
+
+        public Cerchione Cerchione
+        {
+            get { return _cerchione; }
+            set { _cerchione = value; }
+        }
+
+        public Ruota()
+        {
+            Gomma = new Gomma();
+            Cerchione = new Cerchione();
+        }
+
+        public Ruota(Cerchione cerchione, Gomma gomma)
+        {
+            Cerchione = cerchione;
+            Gomma = gomma;
+        }
+
+        public Ruota(Ruota vRuota)
+        {
+            Gomma = vRuota.Gomma;
+            Cerchione = vRuota.Cerchione;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ruota) || obj == null)
+            {
+                return false;
+            }
+
+            Ruota altro = (Ruota)obj;
+            return Gomma == altro.Gomma && Cerchione == altro.Cerchione;
+        }
+
+        public void Aggiunta(IComposite component)
+        {
+
+        }
+
+        public void Rimuovi(int index)
+        {
+
+        }
+
+        public IComposite GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Dettagli gomma: {Gomma}; Dettagli cerchioni: {Cerchione}";
+        }
+
+        public double Prezzo()
+        {
+            return 0;
+        }
+    }
 }
